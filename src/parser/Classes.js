2f5d334cf6eae0fb05fbbbd9d59fa8cf6f8c0259
Ferdinand Schimana <ferdi@Ferdinands-MacBook-Pro.local>
2021-05-03 19:05:34 +0200

    add functioning parser

--- src/parser/Classes.js
+++ src/parser/Classes.js
@@ -1,40 +1,35 @@
+
+
 class Title {
     constructor(value) {
-      this.value = "{{"
-      this.value += value
-      this.value += "}}"
+      this.value = "{{ "
+      this.value += FormatText(value)
+      this.value += " }}"
+      this.value += "\n"
     }
 }
 class Heading {
     constructor(hirarchy, value) {
-      this.value = "=".repeat(hirarchy+1);
-      this.value += value;
-      this.value += "=".repeat(hirarchy+1);
+        this.value = "=".repeat(hirarchy+1)+" "
+        this.value += FormatText(value)+" "
+        this.value += "=".repeat(hirarchy+1)
+        this.value += "\n"
     }
 }
 class Week{
-    constructor(week, value) {
+    constructor(start, end, value) {
         this.value = "'''"
-        this.value += week
+        this.value += FormatDate(start)+" - "+FormatDate(end)
         this.value += "''' \n "
-        this.value += value
+        this.value += FormatText(value)
+        this.value += "\n"
+      }
+}
+class Text{
+    constructor(value) {
+        this.value = FormatText(value)
+        this.value += "\n"
       }
 }
 
-function FormatDate(date){
-    //Date format YYYY-MM-DD
-    //replace '-' ex: 2019-12-11 to 2019.12.11
-    let newDate = date.replace( /-/gi, ".")
-    //get rid of YYYY ex: 2019.12.11 to .12.11 
-    newDate = newDate.substring(4,newDate.length)
-    //change order of '.'MM'.'DD  ex: .12.11 to 11.12.
-    newDate = newDate.replace(/(\.)(\d{2})(\.)(\d{2})/,"$4$3$2$1")
-    return newDate
-}
-
-function FormatText(text){
-    let newText = text.replace(/\*/gi,"'''")
-    newText = newText.replace( /\_/gi,"''")
-    newText = newText.replace()
-    console.log(newText)
-}
+module.exports = {Title, Heading, Week, Text}
---
+++ src/parser/parser.test.js
@@ -0,0 +1,8 @@
+const functions = require('./parser')
+const { ImportJournal } = require('./parser.mjs')
+
+test('imports json with one of each object',() =>{
+    expect ImportJournal(json).toBe(
+        
+    )
+})
