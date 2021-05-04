dfbb80ce70f570e6a531166d298f74f5f3f115a0
Ferdinand Schimana <ferdi@Ferdinands-MacBook-Pro.local>
2021-05-04 11:33:49 +0200

    added tests, fixed space bug, fixed import bug

--- src/parser/Classes.js
+++ src/parser/Classes.js
@@ -1,5 +1,3 @@
-
-
 class Title {
     constructor(value) {
       this.value = "{{ "
@@ -20,7 +18,7 @@ class Week{
     constructor(start, end, value) {
         this.value = "'''"
         this.value += FormatDate(start)+" - "+FormatDate(end)
-        this.value += "''' \n "
+        this.value += "'''\n"
         this.value += FormatText(value)
         this.value += "\n"
       }
@@ -32,4 +30,30 @@ class Text{
       }
 }
 
-module.exports = {Title, Heading, Week, Text}
+function FormatDate(date){
+    //Date format YYYY-MM-DD
+    //replace '-' ex: 2019-12-11 to 2019.12.11
+    let newDate = date.replace( /-/gi, ".")
+    //get rid of YYYY ex: 2019.12.11 to .12.11 
+    newDate = newDate.substring(4,newDate.length)
+    //change order of '.'MM'.'DD  ex: .12.11 to 11.12.
+    newDate = newDate.replace(/(\.)(\d{2})(\.)(\d{2})/,"$4$3$2$1")
+    return newDate
+}
+
+function FormatText(text){
+    let linkReg = /\[(.*?)\]/g
+    let newText = text.replace(/\*/gi,"'''")
+    newText = newText.replace( /\_/gi,"''")
+    let links = newText.match(linkReg)
+    if(links != null){
+        links.forEach(link =>{
+            let newlink = link.substr(0,link.length-1)
+            newlink += " "+newlink.substr(link.indexOf(".")+1,newlink.lastIndexOf(".")-link.indexOf(".")-1)+"]"
+            newText = newText.substr(0,newText.indexOf(link)) + newlink + newText.substr(newText.indexOf(link)+link.length, newText.length) 
+        })
+    }
+    return newText;
+}
+
+module.exports = {Title, Heading, Week, Text, FormatDate, FormatText}
