bcbb9b556f3147022eeb1d255b520dac8e6a1763
Ferdinand Schimana <ferdi@Ferdinands-MacBook-Pro.local>
2021-05-06 17:50:12 +0200

    map

--- src/components/StreetMap/POIIcon.js
+++ src/components/StreetMap/POIIcon.js
@@ -1,14 +1,11 @@
 import L from 'leaflet';
 import icon from './room_white_24dp.svg';
 
+
+
 const iconPerson = new L.Icon({
     iconUrl: icon,
     iconRetinaUrl: icon,
-    iconAnchor: null,
-    popupAnchor: null,
-    shadowUrl: null,
-    shadowSize: null,
-    shadowAnchor: null,
     iconSize: new L.Point(30, 30),
     className: 'leaflet-div-icon'
 });
--- src/components/StreetMap/StreetMap.js
+++ src/components/StreetMap/StreetMap.js
@@ -4,20 +4,33 @@ import "leaflet/dist/leaflet.css"
 import "./index.css"
 import styles from './StreetMap.module.scss';
 import {MapContainer, TileLayer, Popup, Marker } from "react-leaflet"
+import {Link} from "react-router-dom"
 import map from "./map-provider"
-import MarkerShadow from 'leaflet/dist/images/marker-shadow.png';
-import Markericon from 'leaflet/dist/images/marker-icon.png';
 import { iconPerson } from './POIIcon.js';
 import icon from './room_white_24dp.svg';
 import L, { Point, DivIcon } from 'leaflet'
+import { scryRenderedComponentsWithType } from 'react-dom/test-utils';
 
-
-L.Icon.Default.mergeOptions({
-    iconRetinaUrl: Markericon,
-    iconUrl: Markericon,
-    shadowUrl:MarkerShadow
-});
-
+const companies =  [
+  {
+      name: 'Google',
+      website: 'https://www.google.com',
+      departments: ['Hebamme', 'MMT', 'MMA'],
+      salary: 'None',
+      satisfaction: 'Very',
+      links: ['https://wiki.mediacube.at/wiki/index.php?title=Multimediaprojekt_2_Web_-_SS_2021#PraktikumsTagebuch', 'https://wiki.mediacube.at/wiki/index.php?title=Multimediaprojekt_2_Web_-_SS_2021#Voraussetzungen'],
+      position: [48.200364, 14.271086]
+  },
+  {
+      name: 'Google 2',
+      website: 'www.google.com',
+      departments: ['MMT', 'MMA'],
+      salary: 'None',
+      satisfaction: 'Very',
+      links: ['https://wiki.mediacube.at/wiki/index.php?title=Multimediaprojekt_2_Web_-_SS_2021#PraktikumsTagebuch', 'https://wiki.mediacube.at/wiki/index.php?title=Multimediaprojekt_2_Web_-_SS_2021#Voraussetzungen'],
+      position: [49.200364, 14.271086]
+  }
+]
 const StreetMap = () => {
   const [center, setCenter] = useState({ lat: 47.811195, lng: 13.033229})
   const ZOOM_LEVEL = 13
@@ -27,11 +40,16 @@ const StreetMap = () => {
     <div className={styles.StreetMap}>
         <MapContainer center={center} zoom={ZOOM_LEVEL} ref={mapRef}>
           <TileLayer url={map.maptiler.url} attribution={map.maptiler.attribution}/>
-          <Marker position={position} icon={iconPerson}>
-            <Popup>
-              Fh Schnalzburg <br /> Puff bei Hallein
-            </Popup>
-          </Marker>
+            {companies.map((companie,index)=>{
+              return (<Marker position={companie.position} icon={iconPerson}>
+               <Popup><h2>{companie.name}</h2>
+                  <a href={companie.website}>{companie.website}</a>
+                  <p><i class="material-icons">public</i>{companie.departments.join(', ')}</p>
+                  <p>{companie.salary}</p><p>{companie.satisfaction}</p>
+                  <p>{companie.links.map((e, index)=>{return<a href={e}>{e}</a>})}</p>
+                </Popup>
+              </Marker>)
+            })}
         </MapContainer>
     </div>
   )};
